cd46ee8e3a8e47a3167983dd1ecaf7c3b12486e3
Qutbii <kotba323@gmail.com>
2024-07-31 23:44:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    if (index >= totalSlides) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = totalSlides - 1;
+    } else {
+        slideIndex = index;
+    }
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+function changeSlide(direction) {
+    showSlide(slideIndex + direction);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+});
